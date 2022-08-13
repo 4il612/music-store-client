2836fc51d7df27a4b4c457381e2d99edbbe58d20
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-13 12:59:21 +0300

    feat: main page + sidemenu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button } from '@mui/material'
+import NavBar from '../components/NavBar'
 
 const Index = () => {
-    return <div>
-        Hello world!
-        <Button>KEKW</Button>
-    </div>
+    return <>
+        <NavBar/>
+        <div className='center'>
+            <h1>Welcome to music-store</h1>
+            <h3>There are best tracks special for you!</h3>
+            <Button>Kekw</Button>
+        </div>
+        
+        <style jsx>
+            {`
+                .center{
+                    margin-top: 200px;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    flex-direction: column;
+                }
+            `}
+        </style>
+    </>
 }
 
 export default Index
