8c389ff0fd86735330b4db859b16a64d306a1a1f
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-14 22:51:11 +0300

    fix: scss styles of TrackItem

--- components/TrackItem.tsx
+++ components/TrackItem.tsx
@@ -1,5 +1,8 @@
 import { ITrack }  from "../types/Track"
-import {Card} from '@mui/material'
+import {Card, IconButton, Grid} from '@mui/material'
+import styles from '../styles/TrackItem.module.scss'
+import { Delete, Pause, PlayArrow } from "@mui/icons-material"
+import Image from "next/image"
 
 type TrackItemProps = {
     track: ITrack
@@ -8,8 +11,32 @@ type TrackItemProps = {
 
 const TrackItem: React.FC<TrackItemProps> = ({track, active = false}) => {
     return (
-        <Card>
-            {track.title}
+        <Card className={styles.track}>
+            <IconButton>
+                {!active ? <PlayArrow/> : <Pause/>}
+            </IconButton>
+            <Image width={70} height={70} src={track.preview}/>
+            <Grid container direction='column'
+                style={{
+                    width: 200, margin: '0 20px'
+                }}
+            >
+                <div>
+                    {track.title}
+                </div>
+                <div
+                    style={{
+                        fontSize: 14,
+                        color: 'gray'
+                    }}
+                >
+                    {track.author}
+                </div>
+            </Grid>
+            {active && <div>13:37 / 14:88</div>}
+            <IconButton style={{marginLeft: 'auto'}}>
+                <Delete/>
+            </IconButton>
         </Card>
     )
 }
