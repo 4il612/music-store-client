683bd9917b8fb277e22141aa6866e314cd052037
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-20 17:21:13 +0300

    feat: StepperMenu of track creating page

---
+++ components/StepWarapper.tsx
@@ -0,0 +1,33 @@
+import { Card, Container, Grid, Step, StepLabel, Stepper } from "@mui/material"
+import React from "react"
+
+interface SWProps{
+    activeStep: number
+    children: React.ReactNode
+}
+
+const steps = ['Track info', 'Track preview', 'Track audio']
+
+const StepWrapper: React.FC<SWProps> = ({activeStep, children}) => {
+    return (
+        <Container>
+            <Stepper activeStep={activeStep}>
+                {steps.map((step, index) =>
+                    <Step
+                        key={index}
+                        completed={activeStep > index}
+                    >
+                        <StepLabel>{step}</StepLabel>
+                    </Step>
+                )}
+            </Stepper>
+            <Grid container justifyContent='center' style={{margin: '70px 0', height: 270}}>
+                    <Card style={{width: 600}}>
+                        {children}
+                    </Card>
+            </Grid>
+        </Container>
+    )
+}
+
+export default StepWrapper
--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,12 +1,45 @@
-import { Grid, Card, Button, Box } from "@mui/material"
-import { useRouter } from "next/router"
+import { Grid, Button, TextField } from "@mui/material"
+import { useState } from "react"
+import StepWrapper from "../../components/StepWarapper"
 import MainLayout from "../../layout/MainLayout"
 
 const Create = () => {
+    const [activeStep, setActiveStep] = useState(0)
+    
+    const next = () => {
+        setActiveStep(prev => prev + 1)
+    }
+    
+    const back = () => {
+        setActiveStep(prev => prev - 1)
+    }
+
     return (
-        <>
-            good
-        </>
+        <MainLayout>
+            <StepWrapper activeStep={activeStep}>
+                {activeStep === 0 &&
+                    <Grid container direction='column' style={{padding: 20}}>
+                        <TextField
+                            style={{marginBottom: 10}}
+                            label='Track title'
+                        />
+                        <TextField
+                            style={{marginBottom: 10}}
+                            label='Author name'
+                        />
+                        <TextField
+                            label='Lyrics'
+                            multiline
+                            rows={10}
+                        />
+                    </Grid>
+                }
+            </StepWrapper>
+            <Grid container justifyContent='space-between'>
+                <Button disabled={activeStep < 1} onClick={back}>back</Button>
+                <Button disabled={activeStep > 2} onClick={next}>next</Button>
+            </Grid>
+        </MainLayout>
     )
 }
 
