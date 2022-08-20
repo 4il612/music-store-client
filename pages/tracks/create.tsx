ef830475ddd5c6cd0abfa51e21c57de02d557f45
Nikita Shilin <shilin.nick@yandex.ru>
2022-08-20 18:06:05 +0300

    feat: Track creating page STEP 2-3 | need fix: TS linting

--- pages/tracks/create.tsx
+++ pages/tracks/create.tsx
@@ -1,11 +1,14 @@
 import { Grid, Button, TextField } from "@mui/material"
 import { useState } from "react"
+import FileLoader from "../../components/FileLoader"
 import StepWrapper from "../../components/StepWarapper"
 import MainLayout from "../../layout/MainLayout"
 
 const Create = () => {
     const [activeStep, setActiveStep] = useState(0)
-    
+    const [preview, setPreview] = useState('')
+    const [audio, setAudio] = useState('')
+
     const next = () => {
         setActiveStep(prev => prev + 1)
     }
@@ -30,9 +33,23 @@ const Create = () => {
                         <TextField
                             label='Lyrics'
                             multiline
-                            rows={10}
+                            rows={4}
                         />
                     </Grid>
+                }
+                {activeStep === 1 &&
+                    <FileLoader accept='image/*' setFile={setPreview}>
+                        <Button>
+                            ВЫБРАТЬ ИЗОБРАЖЕНИЕ
+                        </Button>
+                    </FileLoader>
+                }
+                {activeStep === 2 &&
+                    <FileLoader accept='audio/*' setFile={setAudio}>
+                        <Button>
+                            ВЫБРАТЬ АУДИО
+                        </Button>
+                    </FileLoader>
                 }
             </StepWrapper>
             <Grid container justifyContent='space-between'>
